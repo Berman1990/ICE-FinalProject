d3e9a74496f012d768bfa5c934da4aa996380ecc
gadi gomez <gadi gomez>
2016-03-09 13:52:55 +0200

    added new project

--- controllers/home.js
+++
@@ -1,8 +0,0 @@
-/**
- * GET /
- * Home page.
- */
-exports.index = function(req, res) {
-  var path = require('path');
-    res.sendFile(path.join(__dirname, '../public', 'main.html'));
-};
---
+++ server/server.js
@@ -0,0 +1,30 @@
+var path = require('path');
+var bodyParser = require('body-parser');
+var express = require('express');
+var logger = require('morgan');
+var mongoose = require('mongoose');
+var config = require('./config');
+
+mongoose.connect(config.MONGO_URI);
+mongoose.connection.on('error', function() {
+  console.error('MongoDB Connection Error. Please make sure that MongoDB is running.');
+});
+
+var app = express();
+
+app.set('port', process.env.PORT || 3000);
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// serve the static client files
+app.use(express.static(path.join(__dirname, '../client')));
+
+// set up the routes
+require('./routes')(app);
+
+// start the server
+app.listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + app.get('port'));
+});
