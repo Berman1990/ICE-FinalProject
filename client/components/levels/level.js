72ec35568832bd3fada7cd9975c25f19225b5167
Kobi Hay <Kobi Hay>
2016-04-27 17:40:10 +0300

    resetGame

--- client/components/levels/level.js
+++ client/components/levels/level.js
@@ -32,35 +32,12 @@ angular.module('myApp.level', ['myApp.level.chart', 'myApp.level.player'])
     $scope.controls = {};
 
     $scope.rotate = function() {
-
-        var wall = new Object();
-        var startPnt = new Object();
-        startPnt.x =0;
-        startPnt.y =200;
-        var endPnt = new Object();
-        endPnt.x=50;
-        endPnt.y=200;
-        wall.startPnt = startPnt;
-        wall.endPnt = endPnt;
-
-
-        var wall2 = new Object();
-        var startPnt2 = new Object();
-        startPnt2.x =400;
-        startPnt2.y =0;
-        var endPnt2 = new Object();
-        endPnt2.x=400;
-        endPnt2.y=200;
-        wall2.startPnt = startPnt2;
-        wall2.endPnt = endPnt2;
-        $scope.controls.DrawWalls([wall,wall2]);
-
-        //$scope.controls.setCommandArray([2,1,2,1,2,1,2,1]);
-        $scope.controls.setCommandArray([2,2,1,1,1,1,1,1,1,1,1]);
+        $scope.controls.setCommandArray([3,1,1,1,1,1]);
     }
 
     $http.get('api/stages/4').success(function (data) {
         $scope.level = data;
+        $scope.controls.ResetGame(data[0],data[0].objects);
     });
 
 
