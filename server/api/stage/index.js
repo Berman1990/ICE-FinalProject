5827ea2541ded0ff7fdaae0a122d44a758cc19bd
gadi gomez <gadi gomez>
2016-04-27 19:43:14 +0300

    finished logic?

--- server/api/stage/index.js
+++ server/api/stage/index.js
@@ -12,51 +12,64 @@ function validationError(res, err) {
 /*
  * Return all stages
  * */
-router.get('/', function(req, res, next) {
-    var query = Stage.find({}).select({ "id": 1, "name": 1, "difficulty": 1, "description": 1, "_id": 0 });
+router.get('/', function (req, res, next) {
+    var query = Stage.find({}).select({"id": 1, "name": 1, "difficulty": 1, "description": 1, "_id": 0});
 
-    query.exec(function(err, stages) {
+    query.exec(function (err, stages) {
         if (err) return next(err);
         res.send(stages);
     });
 });
 
-router.post('/compile', function(req, res) {
+router.post('/compile', function (req, res) {
     var graph = req.body;
 
     var commandsArray = [];
 
-    var tempNode = {};
-
-
     var currentNode = _.findWhere(graph.linkDataArray, {from: "S"});
 
-    while (currentNode.to !== "E") {
+    readGraph(currentNode, "E");
 
-      if(currentNode[0] === "F") {
-        commandsArray.push(1);
-      }
-      else if (currentNode[0] === "R") {
-        commandsArray.push(2);
-      }
-      else if (currentNode[0] === "L") {
-        commandsArray.push(3);
-      }
+    function readGraph(currentNode, stop) {
 
-      currentNode = _.findWhere(graph.linkDataArray, {from: currentNode.to});
+        if (currentNode.to[0] === "F") {
+            commandsArray.push(1);
+        }
+        else if (currentNode.to[0] === "R") {
+            commandsArray.push(2);
+        }
+        else if (currentNode.to[0] === "L") {
+            commandsArray.push(3);
+        }
+        else if (currentNode.to[0] === "K" && stop !== currentNode.to) {
+
+            var loopNode = _.findWhere(graph.linkDataArray, {from: currentNode.to, fromPort: "R"});
+            var i;
+            for (i = 0; i < 2; i++) {
+                readGraph(loopNode, currentNode.to);
+            }
+
+        }
+
+        if (currentNode.to !== stop) {
+
+            readGraph(_.findWhere(graph.linkDataArray, {from: currentNode.to, fromPort: "B"}),stop);
+        }
 
     }
 
 
+    res.send(commandsArray);
 
-});
+})
+;
 
 
 /*
  * Return stage by Id
  * */
-router.get('/:stageId', function(req, res) {
-    Stage.find({ "id": req.param('stageId') }, function(err, stage) {
+router.get('/:stageId', function (req, res) {
+    Stage.find({"id": req.param('stageId')}, function (err, stage) {
         res.send(stage);
     });
 });
