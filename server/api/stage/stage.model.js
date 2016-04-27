0bb9a8c9f407ec131e57bc04cc4a503bc7cb08cf
Berman1990 <Berman1990@gmail.com>
2016-04-27 11:38:57 +0300

    db

--- server/api/stage/stage.model.js
+++ server/api/stage/stage.model.js
@@ -1,10 +1,34 @@
 var mongoose = require('mongoose');
 
 var stageSchema = new mongoose.Schema({
-  id: { type: String, unique: true }
+  id: { type: String, unique: true },
+  name: String,
+  difficulty: Number,
+  description: String,
+  startpoint: String,
+  endpoint: String,
+  objects: [String],
+  commands: [String]
 });
 
+stageSchema.methods.getAllStages = function(){
+ var Stages = mongoose.model('Stages', stageSchema);
+    var stages = Stages.find({});
+  //return StagesList;
+};
 
+stageSchema.getTest = function() {
+  console.log("Hello World");
+};
 
+stageSchema.methods.getTest = function() {
+  console.log("Hello World");
+};
+
+stageSchema.methods.getStageById = function(stageId){
+  var Stages = mongoose.model('Stages', stageSchema);
+  var Stage = Stages.findOne({'id': stageId});
+  return Stage;
+};
 
 module.exports = mongoose.model('Stage', stageSchema);
