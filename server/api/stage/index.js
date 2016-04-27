3337e9fae8d68e245d57be53f330dbcc72377997
gadi gomez <gadi gomez>
2016-04-27 18:34:47 +0300

    added underscore and some logic

--- server/api/stage/index.js
+++ server/api/stage/index.js
@@ -1,27 +1,54 @@
 var express = require('express');
+var _ = require("underscore");
 var router = express.Router();
 
 var auth = require('../../auth/auth.service');
 var Stage = require('./stage.model');
 
 function validationError(res, err) {
-  res.status(422).json(err);
+    res.status(422).json(err);
 }
 
 /*
-* Return all stages
-* */
-router.get('/',  function(req, res, next) {
-  var query = Stage.find({}).select({ "id" : 1,"name": 1,"difficulty" : 1, "description": 1, "_id": 0});
+ * Return all stages
+ * */
+router.get('/', function(req, res, next) {
+    var query = Stage.find({}).select({ "id": 1, "name": 1, "difficulty": 1, "description": 1, "_id": 0 });
 
-  query.exec(function (err, stages) {
-    if (err) return next(err);
-    res.send(stages);
-  });
+    query.exec(function(err, stages) {
+        if (err) return next(err);
+        res.send(stages);
+    });
 });
 
 router.post('/compile', function(req, res) {
-	var data = req.body;
+    var graph = req.body;
+
+    var commandsArray = [];
+
+    var tempNode = {};
+
+
+    var currentNode = _.findWhere(graph.linkDataArray, {from: "S"});
+
+    while (currentNode.to === "E") {
+
+      if(currentNode[0] === "F") {
+        commandsArray.push(1);
+      }
+      else if (currentNode[0] === "R") {
+        commandsArray.push(2);
+      }
+      else if (currentNode[0] === "L") {
+        commandsArray.push(3);
+      }
+
+      currentNode = _.findWhere(graph.linkDataArray, {from: currentNode.to});
+
+    }
+
+
+
 });
 
 
@@ -29,9 +56,9 @@ router.post('/compile', function(req, res) {
  * Return stage by Id
  * */
 router.get('/:stageId', function(req, res) {
-  Stage.find({"id" : req.param('stageId')},function(err, stage) {
-    res.send(stage);
-  });
+    Stage.find({ "id": req.param('stageId') }, function(err, stage) {
+        res.send(stage);
+    });
 });
 
 module.exports = router;
